00be057b1cd79ceedc554ab1a53358ecce92a8f2
ssKyryloss <143904887+ssKyryloss@users.noreply.github.com>
2023-12-24 22:21:48 +0200

    .

--- js/task-4.js
+++ js/task-4.js
@@ -1,9 +1,9 @@
 `use strict`
-const getTotalBalanceByGender = (users, gender) => {
-  return users
+const getTotalBalanceByGender = (users, gender) => 
+  users
     .filter(user => user.gender === gender)
     .reduce((totalBalance, user) => totalBalance + user.balance, 0);
-};
+
 
 const allUsers = [
     {
